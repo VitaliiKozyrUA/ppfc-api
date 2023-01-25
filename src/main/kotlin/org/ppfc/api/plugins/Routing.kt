f24453e897ca6457d304edcdc2964ee4efc6ddbf
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-01-25 17:24:27 +0200

    Refactor: edit text

--- src/main/kotlin/org/ppfc/api/plugins/Routing.kt
+++ src/main/kotlin/org/ppfc/api/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Welcome to PPFC Api Server!")
+            call.respondText("Welcome to PPFC Server!")
         }
     }
 }
