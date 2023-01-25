243629365b52d6f244e587e8e27bcbc80652926c
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-01-25 17:23:32 +0200

    Refactor: expiring cache

--- src/main/kotlin/org/ppfc/api/common/cache/ExpiringCache.kt
+++ src/main/kotlin/org/ppfc/api/common/cache/ExpiringCache.kt
@@ -1,23 +1,9 @@
 package org.ppfc.api.common.cache
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.util.concurrent.ConcurrentHashMap
 
 class ExpiringCache<K, V>(private val expiryTime: Long) : Cache<K, V> {
-    private val clearTime = 300_000L
     private val hashMap = ConcurrentHashMap<K, ExpiringCacheValue<V>>()
-
-    init {
-        CoroutineScope(Dispatchers.Default).launch {
-            while (true) {
-                delay(clearTime)
-                hashMap.clear()
-            }
-        }
-    }
 
     override suspend fun getValue(key: K, block: suspend () -> V?): V? {
         val cachedValue = hashMap[key]
