3b330c2a3ac5e5de60b9746ccb3545939c997d58
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-01-26 05:28:15 +0200

    Feat: add api documentation

--- src/main/kotlin/org/ppfc/api/plugins/ApiRouting.kt
+++ src/main/kotlin/org/ppfc/api/plugins/ApiRouting.kt
@@ -2,11 +2,14 @@ package org.ppfc.api.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import org.ppfc.api.routes.*
 
 fun Application.configureApiRouting() {
     routing {
+        resource("/api", "api-doc.html")
+
         route("/api") {
             authRouting()
 
