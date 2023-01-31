b7222b85735423736b78b97c8eddc53226832ca3
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-01-31 05:13:47 +0200

    Feat: add CORS configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
--- src/main/kotlin/org/ppfc/api/Application.kt
+++ src/main/kotlin/org/ppfc/api/Application.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused")
 fun Application.module() {
     configureDependencyInjection()
+    configureCors()
     configureSerialization()
     configureSecurity()
     configureRouting()
